89785eb0faceb1d13fcb45e7e4b26237acdf92ca
Oviedo F <oviedofederico039@gmail.com>
2022-09-10 16:06:06 -0300

    cloud database

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb://localhost/ovdev-course`)
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sf2fmbz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(uri)
     .then(res => console.log('conectado correctamente :D'))
     .catch(error => console.log(error));
 
